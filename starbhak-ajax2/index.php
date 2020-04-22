10415dd638ef11b8af12160a7fa9a549810a8394
Imam Alfarizi Syahputra <63343105+ImamAlfariziSyahputra@users.noreply.github.com>
2020-04-22 09:54:15 +0700

    Membuat CRUD AJAX tanpa loading (Tahap Pertama)
    tahap 1

---
+++ starbhak-ajax2/index.php
@@ -0,0 +1,135 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css">
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
+    <link rel="stylesheet" href="https://cdn.datatables.net/1.10.20/css/dataTables.bootstrap.min.css">
+</head>
+<body>
+    <div class="table-responsive">
+        <table id="data" class="table table-striped table-bordered" cellspacing="0" style="width:100%">
+            <thead>
+                <tr>
+                    <td>No</td>
+                    <td>Nama Siswa</td>
+                    <td>Alamat</td>
+                    <td>Jurusan</td>
+                    <td>Jenis Kelamin</td>
+                    <td>Tanggal Masuk</td>
+                    <td>Aksi</td>
+                </tr>
+            </thead>
+            <tbody>
+                <?php 
+                    include 'koneksi.php';
+                    $no = 1;
+                    $query = "SELECT * FROM tbl_siswa_search ORDER BY id DESC";
+                    $dewan1 = $db1->prepare($query);
+                    $dewan1->execute();
+                    $res1 = $dewan1->get_result();
+                    while ($row = $res1->fetch_assoc()) {
+                        $id = $row['id'];
+                        $nama_siswa = $row['nama_siswa'];
+                        $alamat = $row['alamat'];
+                        $jurusan = $row['jurusan'];
+                        $jenis_kelamin = $row['jenis_kelamin'];
+                        $tgl_masuk = $row['tgl_masuk'];
+                ?>                
+                <tr>
+                    <td><?= $no++; ?></td>
+                    <td><?= $nama_siswa; ?></td>
+                    <td><?= $alamat; ?></td>
+                    <td><?= $jurusan; ?></td>
+                    <td><?= $jenis_kelamin; ?></td>
+                    <td><?= $tgl_masuk; ?></td>
+                    <td>
+                        <button style='font-size: 11px;' class='btn btn-primary' id='detail' name='detail' title='Lihat Detail'><i class="fa fa-search"></i></button>
+                    </td>
+                </tr>
+                <?php } ?>
+            </tbody>
+        </table>
+    </div>    
+
+    <div id="viewModal" class="modal fade mr-tp-100" role="dialog">
+        <div class="modal-dialog modal-lg flipInx animated">
+            <div class="modal-content">
+                <div class="modal-header">
+                    <h4 class="modal-title" id="myModalLabel">View Data</h4>
+                    <button type="button" class="close" data-dismiss="modal">
+                        <span aria-hidden="true">&times;</span>
+                        <span class="sr-only">Close</span>
+                    </button>
+                </div>
+                <div class="modal-body">
+                    <div class="form-group">
+                        <label>ID</label>
+                        <input type="text" id="id" class="form-control" readonly="">
+                    </div>
+                    <div class="form-group">
+                        <label>Nama Mahasiswa</label>
+                        <input type="text" id="nama_siswa" class="form-control" readonly="">
+                    </div>
+                    <div class="form-group">
+                        <label>Alamat</label>
+                        <input type="text" id="alamat" class="form-control" readonly="">
+                    </div>
+                    <div class="form-group">
+                        <label>Jurusan</label>
+                        <input type="text" id="jurusan" class="form-control" readonly="">
+                    </div>
+                    <div class="form-group">
+                        <label>Jenis Kelamin</label>
+                        <input type="text" id="jenis_kelamin" class="form-control" readonly="">
+                    </div>
+                    <div class="form-group">
+                        <label>Tanggal Masuk</label>
+                        <input type="text" id="tgl_masuk" class="form-control" readonly="">
+                    </div>
+                </div>
+                <div class="modal-footer">
+                    <button class="btn btn-default" data-dismiss="modal">
+                        Close
+                    </button>
+                </div>
+            </div>
+        </div>
+    </div>
+
+
+
+
+<script src="https://kit.fontawesome.com/bb601c2381.js" crossorigin="anonymous"></script>
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
+<script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js"></script>
+<script src="https://cdn.datatables.net/1.10.19/js/jquery.dataTables.min.js"></script>
+<script src="https://cdn.datatables.net/1.10.19/js/dataTables.bootstrap4.min.js"></script>
+<script type="text/javascript">
+    $(document).ready(function() {
+        let table = $('#data').DataTable();
+
+        $('#data tbody').on('click', '#detail', function() {
+            var current_row = $(this).parents('tr');
+            if (current_row.hasClass('child')) {
+                curren_row = current_row.prev();
+            }
+            var data = table.row(current_row).data();
+            console.log(data)
+
+            document.getElementById("id").value = data[0];
+            document.getElementById("nama_siswa").value = data[1];
+            document.getElementById("alamat").value = data[2];
+            document.getElementById("jurusan").value = data[3];
+            document.getElementById("jenis_kelamin").value = data[4];
+            document.getElementById("tgl_masuk").value = data[5];
+
+            $("#viewModal").modal("show");
+        });
+
+    });
+</script>
+</body>
+</html>
